46b43f0607c1a496a7d18513fd59467e9657bdd1
rypall <sim.pierre91@gmail.com>
2017-12-08 00:02:25 +0100

    Update main.c

--- sources/c/main.c
+++ sources/c/main.c
@@ -1,6 +1,9 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "../h/struct.h"
 #include "../h/some_funct.h"
 #include "../h/parserYAML.h"
-
 
 /* ----- GLOBALS ----- */
 int FILE_LINE_COUNTER;
