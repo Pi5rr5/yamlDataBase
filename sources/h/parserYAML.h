0acc2c9f5104ad1a305bc6f4bce5cc643809c03d
HugoESGI <33398894+HugoESGI@users.noreply.github.com>
2017-12-17 12:00:35 +0100

    Update parserYAML.h

--- sources/h/parserYAML.h
+++ sources/h/parserYAML.h
@@ -1,56 +1,34 @@
 #ifndef PARSERYAML_H_INCLUDED
-#define PARSERYAML_H_INCLUDED
+    #define PARSERYAML_H_INCLUDED
+    extern int FILE_LINE_COUNTER;
 
+    listOfLines* addLineToList(listOfLines*, lineStruct);
+    void displayListOfLines(listOfLines*);
+    void freeListOfLines(listOfLines**);
 
-int freadLine(char *, unsigned int, FILE *);
+    listOfEntities* addEntityToList(listOfEntities*, listOfLines*);
+    listOfEntities* removeLastEntity(listOfEntities**);
+    listOfEntities* removeEntityAt(listOfEntities**, int);
+    void displayListOfEntities(listOfEntities*);
+    void freeListOfEntities(listOfEntities**);
 
+    int isEOY(char*);
+    int verifLine(char*);
+    char* getKey(char*);
+    char* getValue(char*);
+    lineStruct getLineStruct(char*);
+    listOfLines* getEntity(int, char*);
 
-extern int FILE_LINE_COUNTER;
+    int compareIntoLine(lineStruct, char*, char*, char*);
+    int compareIntoEntity(listOfLines*, char*, char*, char*);
+    int compareIntoList(listOfEntities*, char*, char*, char*);
 
-listOfLines *addLineToList(listOfLines *, lineStruct);
+    listOfEntities* getBlockWhere(arrayOfStrings, arrayOfStrings, arrayOfStrings, char*);
+    listOfEntities* getAllFrom(char*);
 
-void displayListOfLines(listOfLines *);
-
-void freeListOfLines(listOfLines **);
-
-listOfEntities *addEntityToList(listOfEntities *, listOfLines *);
-
-listOfEntities *removeLastEntity(listOfEntities **);
-
-listOfEntities *removeEntityAt(listOfEntities **, int);
-
-void displayListOfEntities(listOfEntities *);
-
-void freeListOfEntities(listOfEntities **);
-
-int isEOY(char *);
-
-int verifLine(char *);
-
-char *getKey(char *);
-
-char *getValue(char *);
-
-lineStruct getLineStruct(char *);
-
-listOfLines *getEntity(int, char *);
-
-int compareIntoLine(lineStruct, char *, char *, char *);
-
-int compareIntoEntity(listOfLines *, char *, char *, char *);
-
-int compareIntoList(listOfEntities *, char *, char *, char *);
-
-listOfEntities *getBlockWhere(arrayOfStrings, arrayOfStrings, arrayOfStrings, char *);
-
-listOfEntities *getAllFrom(char *);
-
-int updateValuesWhere(AoS, AoS, AoS, AoS, AoS, char *);
-
-int insertLine(lineStruct, char *);
-
-int insertEntity(listOfLines *, char *);
-
-int insertListOfEntities(listOfEntities *, char *);
+    int updateValuesWhere(AoS, AoS, AoS, AoS, AoS, char*);
+    int insertLine(lineStruct, char*);
+    int insertEntity(listOfLines*, char*);
+    int insertListOfEntities(listOfEntities*, char*);
 
 #endif // PARSERYAML_H_INCLUDED
